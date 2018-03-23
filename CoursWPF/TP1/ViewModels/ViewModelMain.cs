00571a1fd91e1fa24609461a25e72ba04ed1b909
dchata <chatagnierdylan@gmail.com>
2018-03-23 13:31:36 +0100

    Appel à la méthode AddItem_Execute pour afficher les TabItem

--- CoursWPF/TP1/ViewModels/ViewModelMain.cs
+++ CoursWPF/TP1/ViewModels/ViewModelMain.cs
@@ -11,23 +11,12 @@ namespace CoursWPF1.ViewModels
 {
     public class ViewModelMain : ViewModelList<ViewModelBase>
     {
-        #region Fields
-        private DelegateCommand _OpenTabItemCommand;
-        #endregion
-
-        #region Properties
-        public DelegateCommand OpenTabItemCommand => _OpenTabItemCommand;
-        #endregion
-
         #region Constructors
-        public ViewModelMain()
-        {
-            _OpenTabItemCommand = new DelegateCommand(OpenTabItemCommand_Execute);
-        }
+        public ViewModelMain() { }
         #endregion
 
         #region Methods
-        protected virtual void OpenTabItemCommand_Execute(object parameter)
+        protected override void AddItem_Execute (object parameter)
         {
             if (parameter is Type viewModelType && viewModelType.GetConstructor(Type.EmptyTypes).Invoke(null) is ViewModelBase viewModel)
             {
