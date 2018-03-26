d70a5198fea47a88f709f9823a2819d4e8f59f6f
dchata <chatagnierdylan@gmail.com>
2018-03-26 16:14:01 +0200

    Ajout main window 2

---
+++ CoursWPF/TP1/MainWindow2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CoursWPF1.ViewModels;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : MetroWindow
+    {
+        public MainWindow2()
+        {
+            DataContext = new ViewModelMain2();
+            InitializeComponent();
+        }
+    }
+}
---
+++ CoursWPF/TP1/ViewModels/ViewModelMain2.cs
@@ -0,0 +1,32 @@
+﻿using MVVMLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMLib.ViewModels;
+
+namespace CoursWPF1.ViewModels
+{
+    public class ViewModelMain2 : ViewModelBase
+    {
+        #region Fields
+        private ViewModelBase _SelectedViewModel;
+        #endregion
+
+        #region Properties
+        public ViewModelBase SelectedViewModel { get => _SelectedViewModel; set => SetProperty(nameof(SelectedViewModel), ref _SelectedViewModel, value); }
+        #endregion
+
+        #region Constructors
+        public ViewModelMain2()
+        {
+            SelectedViewModel = new ViewModelPeople();
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
